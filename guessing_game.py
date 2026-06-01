01b603892e75370ee56ae8dcc4a945ec62e4affb
Manesh <manishbhagane127@gmail.com>
2026-06-01 13:46:17 +0530

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,21 @@
+import random
+number = random.randint(1, 10)
+while True:
+    while True:
+        guess = input("Guess the number \n:")
+        try:
+            guess = int(guess)
+            break
+        except:
+            print("Enter number only!")
+            continue
+
+    if guess == number:
+        print("Congratulations! , You guessed the right number")
+        break
+    elif guess > number:
+        print("📈 Too high! Go lower!")
+    elif guess < number:
+        print("📉 Too low! Go higher!")
+
+S
